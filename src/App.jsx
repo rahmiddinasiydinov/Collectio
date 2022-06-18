6fbe3d48f2d54e32e58d12c00b899459f09771e1
rahmiddinasiydinov <rahmiddinasiydinov@gmail.com>
2022-06-19 01:32:56 +0500

    init:start collectio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className="salom">Hello world</h1>
+    </div>
+    );
+  }
+  
+  export default App;
+  
